652a64651ffe82fa39d21633d03229a635579bbd
SunYang <sunyang@SunYangdeMacBook-Pro.local>
2017-07-31 11:56:10 +0800

    Initial:
    q

---
+++ Package.swift
@@ -0,0 +1,22 @@
+import PackageDescription
+
+let package = Package(
+    name: "FoodTracker",
+    targets: [
+      Target(name: "FoodTracker", dependencies: [ .Target(name: "Application") ]),
+      Target(name: "Application", dependencies: [
+            .Target(name: "Generated"),
+      ]),
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git",             majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",       majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Health.git",             majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2),
+        .Package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", majorVersion: 1),
+
+
+
+    ],
+    exclude: ["src"]
+)
